b2b8c4dabc151a0f3ff6c5ba7475a9ac9812b8c1
Sai SriHarsha Griddaluru <190030545@kluniversity.in>
2020-10-31 20:37:37 +0530

    UI Elements added

--- project-b4eat/b4eat-frontend/src/app/app-routing.module.ts
+++ project-b4eat/b4eat-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortalViewComponent } from './pages/portal-view/portal-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PortalViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ project-b4eat/b4eat-frontend/src/app/pages/portal-view/portal-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portal-view',
+  templateUrl: './portal-view.component.html',
+  styleUrls: ['./portal-view.component.scss']
+})
+export class PortalViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
